1ace5915abe1a3d58eb0058c067d967a1ca280d2
Cesar Canassa <cesar.canassa@gmail.com>
2013-10-29 20:33:29 -0200

    Added rbenv initialization

--- config.fish
+++ config.fish
@@ -21,6 +21,13 @@
 # set __fish_git_prompt_char_upstream_behind '↓'
 
 
+# Initialize rbenv
+if test -d ~/.rbenv
+    set fish_user_paths ~/.rbenv/bin $fish_user_paths
+    . (rbenv init -|psub)
+end
+
+
 # Prevents the Virtual Env activate.fish from messing with my prompt
 set VIRTUAL_ENV_DISABLE_PROMPT 'yes'
 
