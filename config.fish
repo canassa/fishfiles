6bde19f8fd6a64f7ad102d37f456e99b6312a9fc
Cesar Canassa <cesar.canassa@gmail.com>
2014-06-03 01:05:30 -0300

    Sets locale to en_US.UTF-8 on OS X

--- config.fish
+++ config.fish
@@ -6,6 +6,16 @@ if test -d ~/.rbenv
     . (rbenv init -|psub)
 end
 
-
 # Prevents the Virtual Env activate.fish from messing with my prompt
 set VIRTUAL_ENV_DISABLE_PROMPT 'yes'
+
+# Fix OS X broken locales
+if test (uname) = Darwin
+    set -x LANG en_US.UTF-8
+    set -x LC_COLLATE en_US.UTF-8
+    set -x LC_CTYPE en_US.UTF-8
+    set -x LC_MESSAGES en_US.UTF-8
+    set -x LC_MONETARY en_US.UTF-8
+    set -x LC_NUMERIC en_US.UTF-8
+    set -x LC_TIME en_US.UTF-8
+end
