7c6a9ac54fb9ff581821ba2b0691ab20f8935e9c
Cesar Canassa <cesar.canassa@new10.com>
2019-09-01 12:24:38 +0200

    :sparkles: dotenv handles comments now

--- functions/dotenv.fish
+++ functions/dotenv.fish
@@ -6,7 +6,18 @@ function dotenv --description 'Load environment variables from .env file'
 
   if test -e $envfile
     for line in (cat $envfile)
-      set -xg (echo $line | cut -d = -f 1) (echo $line | cut -d = -f 2-)
+        # Splits the line in two on the equal sign
+        set -l entry (string split -m 1 = $line)
+
+        # Skips if the split did not generated two strings
+        if test (count $entry) -eq 2
+            # Skips if the variable name contains a # sign (comment)
+            if test -z (string match -e '#' $entry[1])
+                # echo "$entry[1]=$entry[2]"
+                set -xg $entry[1] $entry[2]
+            end
+        end
+      # set -xg (echo $line | cut -d = -f 1) (echo $line | cut -d = -f 2-)
     end
   end
 end
