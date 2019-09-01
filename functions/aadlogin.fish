05cd7cc2b6be4bad43e2ff34690550ace5e01e2c
Cesar Canassa <cesar.canassa@new10.com>
2019-09-01 12:21:05 +0200

    :wrench: Using the upstream aadlogin

--- functions/aadlogin.fish
+++ functions/aadlogin.fish
@@ -1,3 +1,3 @@
 function aadlogin --description "New 10 AWS login"
-    docker run -ti -v ~/.aws:/root/.aws new10/aadlogin --profile new10 $argv
+    docker run -ti -v ~/.aws:/root/.aws dtjohnson/aws-azure-login --profile new10 $argv
 end
